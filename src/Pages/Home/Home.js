69d2cad0d96af8c1fa7101211c627e0372fd0a5a
ayushkumar32539 <ayush2010128@akgec.ac.in>
2022-12-01 17:38:59 +0530

    image error is removed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar';
 import Slider from '../../Components/Slider/Slider';
@@ -30,7 +30,7 @@ const Home = () => {
             <span>District Wise Bed Availability</span>
         </div>
           <div className='logodiv'>
-          <img src={logo}/>
+          <img src={logo} alt="img"/>
           </div>
         </div>
         </div>
@@ -45,7 +45,7 @@ const Home = () => {
           <Slider/>
           <div className='images'>
             <div className='hospt_img'>
-            <img src='https://images.unsplash.com/photo-1578991624414-276ef23a534f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1578991624414-276ef23a534f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80' alt="img"></img>
             <div className='combine'>
             <div className='hospitalstate'>
             <span className='hosp'>ApolloHospital</span>
@@ -55,7 +55,7 @@ const Home = () => {
             </div>
             
             <div className='hospt_img'>
-            <img src='https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80' alt="img"></img>
             <div className='combine'>
             <div className='hospitalstate'>
             <span className='hosp'>ApolloHospital</span>
@@ -65,7 +65,7 @@ const Home = () => {
             </div>
 
             <div className='hospt_img'>
-            <img src='https://images.unsplash.com/photo-1578991624414-276ef23a534f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1578991624414-276ef23a534f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80' alt="img"></img>
             <div className='combine'>
             <div className='hospitalstate'>
             <span className='hosp'>ApolloHospital</span>
@@ -75,7 +75,7 @@ const Home = () => {
             </div>
 
             <div className='hospt_img'>
-            <img src='https://images.unsplash.com/photo-1578991624414-276ef23a534f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1578991624414-276ef23a534f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80' alt="img "></img>
             <div className='combine'>
             <div className='hospitalstate'>
             <span className='hosp'>ApolloHospital</span>
