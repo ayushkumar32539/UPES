8efeac160ac4cf8b3c208d0abdec461b28281354
ayushkumar32539 <ayush2010128@akgec.ac.in>
2023-02-18 06:27:19 +0530

    leftout changes

--- src/Components/sellerlist/Sellerlist.js
+++ src/Components/sellerlist/Sellerlist.js
@@ -35,11 +35,7 @@ const Sellerlist = ({ email, wasteid, name, mobilenum, city }) => {
     <div className="Hosplist">
       <div className="inHosplist">
         <div className="hosplistimg">
-<<<<<<< HEAD:src/Components/hosplist/Hosplist.js
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/5585/production/_118539812_067369333.jpg" alt="img"></img>
-=======
           <img src="https://images.thequint.com/thequint%2F2018-08%2F79e4c234-cbb9-4b1e-bef1-cfda5fa80289%2FIMG_5379.JPG?auto=format%2Ccompress&fmt=webp&width=720" alt="img"></img>
->>>>>>> ecd990ba968c917df8f4fb55a507aeb81f0436f9:src/Components/sellerlist/Sellerlist.js
         </div>
         <div className="hosplisttxt">
           <div className="hosplisttitle">
@@ -78,13 +74,8 @@ const Sellerlist = ({ email, wasteid, name, mobilenum, city }) => {
             </div>
 
             <div className="line">
-<<<<<<< HEAD:src/Components/hosplist/Hosplist.js
-              <span>Garbage Available:</span>
-              {beds_avail && (
-=======
               <span>Amount : </span>
               {waste_avail && (
->>>>>>> ecd990ba968c917df8f4fb55a507aeb81f0436f9:src/Components/sellerlist/Sellerlist.js
                 <span className="bluetxt">
                   {waste_avail.wasteData.plasticType.availbility}(plastic) +{" "}
                   {waste_avail.wasteData.otherType.availbility}(other)
@@ -99,15 +90,9 @@ const Sellerlist = ({ email, wasteid, name, mobilenum, city }) => {
 
                  {disable === '' && <Button  color="success"sx={{width:"10rem"}} variant="contained"  onClick={(e) => {
               e.preventDefault();
-<<<<<<< HEAD:src/Components/hosplist/Hosplist.js
-              navigate("/bedavailability", { state: { hospid } });
-            }} >Checkout</Button>}
-                 {disable === 'yes' && <Button disabled  color="success"sx={{width:"10rem"}} variant="contained">Checkout</Button>}
-=======
               navigate("/bedavailability", { state: { wasteid } });
             }} >Confirm the purchase</Button>}
                  {disable === 'yes' && <Button disabled  color="success"sx={{width:"10rem"}} variant="contained">Confirm the purchase</Button>}
->>>>>>> ecd990ba968c917df8f4fb55a507aeb81f0436f9:src/Components/sellerlist/Sellerlist.js
 
           </span>
         </div>
